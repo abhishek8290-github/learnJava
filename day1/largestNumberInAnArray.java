101f0259e43c0e5d2edc3b9d6cad5cf6da1fe7c6
Abhishek <abhishek8290.work@gmail.com>
2025-05-14 18:04:09 +0530

    largestNumber

---
+++ day1/largestNumberInAnArray.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class largestNumberInAnArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int largest = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i];
+            }
+        }
+        System.out.println("The largest number is " + largest);
+    }
+}
